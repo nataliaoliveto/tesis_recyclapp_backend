48246fe9794a27cd88bc125a862ec0d0f8d75559
nataliaoliveto <nataliaoliveto94@gmail.com>
2024-11-13 11:14:51 -0300

    feat: benefitassignment

--- controllers/benefitAssignment.controller.ts
+++ controllers/benefitAssignment.controller.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 import type { Request, Response } from "express";
 
 const prisma = new PrismaClient();
